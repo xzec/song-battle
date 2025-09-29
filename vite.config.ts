665198271aae1a3d39f21a17fbaa0a148090a1e2
Juraj Zec <juro.zec.1@gmail.com>
2025-09-29 21:44:43 +0700

    fixed tsconfig path aliases, they are now correctly working with Vite (also IntelliJ auto-import). also included node types in node tsconfig
    Signed-off-by: Juraj Zec <juro.zec.1@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -16,4 +17,9 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
 })
